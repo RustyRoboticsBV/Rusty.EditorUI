2cfa1476e645a4f4dd36d226de2e66b0631cf703
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2025-01-05 21:57:24 +0100

    Added HSeparatorElement.

---
+++ Separators/HSeparatorElement.cs
@@ -0,0 +1,53 @@
+﻿using Godot;
+
+namespace Rusty.EditorUI
+{
+    /// <summary>
+    /// A horizontal separator element.
+    /// </summary>
+	public partial class HSeparatorElement : Element
+    {
+        /* Public properties. */
+        /// <summary>
+        /// The color of the separator.
+        /// </summary>
+        public Color Color
+        {
+            get => Separator.Modulate;
+            set => Separator.Modulate = value;
+        }
+
+        /* Protected properties. */
+        protected HSeparator Separator { get; private set; }
+
+        /* Private properties. */
+        private MarginContainer LabelContainer { get; set; }
+
+        /* Constructors. */
+        public HSeparatorElement() : base() { }
+
+        public HSeparatorElement(float height) : base(height) { }
+
+        public HSeparatorElement(Element other) : base(other) { }
+
+        /* Public methods. */
+        public override Element Duplicate()
+        {
+            return new HSeparatorElement(this);
+        }
+
+        /* Protected methods. */
+        protected override void Init()
+        {
+            // Base element init.
+            base.Init();
+
+            // Add separator.
+            Separator = new();
+            AddChild(Separator);
+
+            // Set default height.
+            Height = 4;
+        }
+    }
+}
